5a842275ca152bb79a11d2f8d4abd9ae49f711df
김용찬 <ycbalor@gmail.com>
2019-03-10 16:01:28 +0900

    윈폼의 Aplication 클래스

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp1
+{
+	class Program : Form
+	{
+		static void Main(string[] args)
+		{
+			Program form = new Program();
+			//form.Click += new EventHandler(Form_Click);
+
+			// 람다식으로 처리
+			form.Click += (sender, e) =>
+			{
+				Console.WriteLine("폼 클릭 이벤트");
+				Application.Exit();
+			};
+
+			Console.WriteLine("윈도우 시작..");
+			Application.Run(form);
+			Console.WriteLine("윈도우 종료..");
+		}
+
+		//static void Form_Click(object sender, EventArgs e)
+		//{
+		//	Console.WriteLine("폼 클릭 이벤트");
+		//	Application.Exit();
+		//}
+	}
+}
